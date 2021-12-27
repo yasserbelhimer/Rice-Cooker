b4328f1912f6167d3e351d02b355719fcb9a8904
yasserbelhimer <yasserbelhimer@gmail.com>
2021-12-27 23:46:03 +0100

    first init

--- src/App.js
+++ src/App.js
@@ -1,23 +1,28 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
+import { useState } from "react";
 
 function App() {
+  const [isOpen, setIsOpen] = useState(true);
+  const [on, setOn] = useState(false);
+  const handleOpen = () => setIsOpen(!isOpen);
+  const handleOn = () => setOn(!on);
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="wrapper">
+      <div className="cooker">
+        <div className={`lid ${isOpen && "hidden"}`}></div>
+        <h1 style={{ marginTop: 40 }}>{on ? "ON" : "OFF"}</h1>
+
+        <div className="butspace">
+          <button className="onn" onClick={handleOn}>
+            <ion-icon id="tr" name="power-outline"></ion-icon>
+          </button>
+          <button className="onn">Timer</button>
+          <button className="onn" onClick={handleOpen}>
+            {isOpen ? "CLOSE" : "OPEN"}
+          </button>
+          <button className="onn">0</button>
+        </div>
+      </div>
     </div>
   );
 }

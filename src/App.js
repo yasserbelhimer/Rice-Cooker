73ac6f2e8c4220431f424a4ee0650b15a3bf853c
yasserbelhimer <yasserbelhimer@gmail.com>
2021-12-29 21:44:52 +0100

    fix timer

--- src/App.js
+++ src/App.js
@@ -7,13 +7,17 @@ function App() {
   const [isStarted, setIsStarted] = useState(false);
   const [temperature, setTemperature] = useState(0);
   const [restTime, setRestTime] = useState(30);
-  const [mode, setMode] = useState(1);
+  const [mode, setMode] = useState(2);
+  const [timer, setTimer] = useState(0);
   const handleOpen = () => {
     if (!isOn) setIsOpen(!isOpen);
   };
+  window.timer = 0;
   const handleOn = () => {
     if (!isOpen && !isStarted) setIsOn(!isOn);
-    setMode(1);
+    setMode(2);
+    setRestTime(30);
+    setTemperature(0);
   };
   const handleMode = () => {
     if (!isStarted) setMode((mode % 3) + 1);
@@ -21,12 +25,35 @@ function App() {
   const handleIsStarted = () => {
     if (isOn) setIsStarted(!isStarted);
     if (!isStarted) {
-      setInterval(setRestTime, 1000,restTime-1);
+      if (temperature === 0) setTemperature(1);
+      var t = setInterval(
+        decriment,
+        1000,
+        (restTime) => restTime - 1,
+        (temperature) => {
+          if (temperature < 120) {
+            return temperature + 5 * mode;
+          } else {
+            return temperature - 3 * mode;
+          }
+        }
+      );
+      setTimer(t);
+
+      setTimeout(() => {
+        clearInterval(t);
+        setIsStarted(false);
+        setRestTime(30);
+        setTemperature(0);
+      }, restTime * 1000);
+    } else {
+      clearInterval(timer);
     }
   };
-  function decriment() {
-    setRestTime(restTime - 1);
-  }
+  const decriment = (t, temp) => {
+    setTemperature(temp);
+    setRestTime(t);
+  };
   return (
     <div className="wrapper">
       <div className="cooker">
@@ -43,13 +70,14 @@ function App() {
           <div className="row mt-3">
             <div className="col">
               <h4 className={`${isOn && mode === 1 && "text-primary"}`}>
-                Rapide
+                Lent
               </h4>
               <h4 className={`${isOn && mode === 2 && "text-primary"}`}>
                 Normal
               </h4>
+
               <h4 className={`${isOn && mode === 3 && "text-primary"}`}>
-                Lent
+                Rapide
               </h4>
             </div>
             <div className="col text-right">
@@ -62,13 +90,17 @@ function App() {
           <button className="onn" onClick={handleOpen} disabled={isOn}>
             {isOpen ? "CLOSE" : "OPEN"}
           </button>
-          <button className="onn" onClick={handleOn} disabled={isOpen}>
+          <button
+            className="onn"
+            onClick={handleOn}
+            disabled={isOpen || isStarted}
+          >
             <span>
               <i className="fas fa-power-off fa-lg"></i>
             </span>
           </button>
 
-          <button className="onn" onClick={handleMode}>
+          <button className="onn" onClick={handleMode} disabled={isStarted}>
             MODE
           </button>
           <button className="onn" onClick={handleIsStarted}>
